670066fbaf80dc590aba0b64cef76bfb814e9961
Jiacan Li <sakurajima@163.com>
2020-11-06 21:13:22 +0800

    旋转的方块

---
+++ 1.RotateForEach/Assets/Scripts/RotateForEachSystem.cs
@@ -0,0 +1,18 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class RotateForEachSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        var deltaTime = Time.DeltaTime;
+
+        Entities.WithName("RotateForEachSystem").ForEach((ref Rotation rotation, in RotateForEach rotationSpeed) =>
+        {
+            rotation.Value = math.mul(
+                math.normalize(rotation.Value),
+                quaternion.AxisAngle(math.up(), rotationSpeed.RadiansPerSecond * deltaTime));
+        }).ScheduleParallel();
+    }
+}
